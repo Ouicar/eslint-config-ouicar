9c3e77e418d735acd164d3ac4f65828f901665d7
adrian-amigues <adrian.amigues@gmail.com>
2020-09-30 12:24:48 +0200

    chore(eslint): upgrade eslint and ts-eslint versions

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -31,6 +31,7 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
